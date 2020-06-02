99dd52578b715028632a99136bf2d816822f9645
Fatih <fatihcan.social@gmail.com>
2020-06-02 13:54:40 +0300

    tailwind build fix

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
-    './src/**/*.js',
     './src/**/*.jsx',
   ],
 
